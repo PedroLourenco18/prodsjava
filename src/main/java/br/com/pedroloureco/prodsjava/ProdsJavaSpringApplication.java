4d1fd322b2213fecfe8f8dfde317ebc627bbc3ac
PedroLourenco18 <pedroedu2007@gmail.com>
2025-02-15 01:27:56 -0300

    first commit

---
+++ src/main/java/br/com/pedroloureco/prodsjava/ProdsJavaSpringApplication.java
@@ -0,0 +1,13 @@
+package br.com.pedroloureco.prodsjava;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProdsJavaSpringApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProdsJavaSpringApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/com/pedroloureco/prodsjava/ProdsJavaSpringApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.pedroloureco.prodsjava;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProdsJavaSpringApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
